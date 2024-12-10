6857a8bbc0208220f77c52407991425cc1da6b05
Mindaugas Zilionis <mindaugas.zilionis@linqo.eu>
2024-12-10 17:47:02 +0200

    .

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,4 @@
+
 let slideIndex = 0;
 showSlides();
 
@@ -10,4 +11,26 @@ function showSlides() {
     if (slideIndex > slides.length) { slideIndex = 1 }    
     slides[slideIndex - 1].style.display = "block";  
     setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const navLinks = document.querySelector(".nav-links");
+  
+    burger.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+    });
+  
+    document.querySelectorAll(".nav-links a").forEach((link) => {
+      link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+      });
+    });
+    // burger is workin
+  });
+
+  function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show'); // Toggle the 'show' class
 }
--- script.js
+++ script.js
@@ -118,16 +118,4 @@ const endDate = "Apr 14, 2025 02:00:00"; // Y time in the future
 // Initialize the progress bar
 updateProgressBar(startDate, endDate);
 
-let slideIndex = 0;
-showSlides();
 
-function showSlides() {
-    let slides = document.getElementsByClassName("mySlides");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }    
-    slides[slideIndex - 1].style.display = "block";  
-    setTimeout(showSlides, 10000); // Change image every 5 seconds
-}
